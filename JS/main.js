a3cfbc7c1eb062dfbc1c0e94b008c34298b4aa22
NasoohOlabi <nasooholabi@gmail.com>
2022-02-12 18:22:26 +0200

    silly simple spinner

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,23 @@
 function SayHi() {
 	alert("Hi");
 }
+
+function main() {
+	window.parsing_input_interval = undefined;
+	for (let elem of document.getElementsByClassName("text-input")) {
+		elem.addEventListener("input", (event) => {
+			console.log(`event`);
+			if (window.parsing_input_interval) {
+				clearInterval(window.parsing_input_interval);
+			}
+			document.getElementById("spinner").style.display = "inline";
+			document.querySelector(".form-block > button").disabled = true;
+			window.parsing_input_interval = setTimeout(() => {
+				document.getElementById("spinner").style.display = "none";
+				document.querySelector(".form-block > button").disabled = false;
+			}, 1000);
+		});
+	}
+}
+
+window.onload = main;
