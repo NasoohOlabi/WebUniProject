2cab99c5b358e867b24ea33679775d06257d5347
NasoohOlabi <nasooholabi@gmail.com>
2022-03-06 06:38:04 +0200

    added filename option

--- application/libs/util/log.php
+++ application/libs/util/log.php
@@ -3,10 +3,10 @@
 // error message to be logged
 // setting error logging to be active
 ini_set("log_errors", TRUE);
-function simpleLog($msg)
+function simpleLog($msg, $filename = null)
 {
     // path of the log file where errors need to be logged
-    $log_file = 'logs/' . date("Y-m-d") . ".log";
+    $log_file = 'logs/' . (($filename === null) ? date("Y-m-d") : $filename) . ".log";
     // setting the logging file in php.ini
     ini_set('error_log', $log_file);
     // logging the error
