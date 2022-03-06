29eea4561c08aca3333b57d8e3c59aa06c3727d7
NasoohOlabi <nasooholabi@gmail.com>
2022-03-06 06:53:49 +0200

    Now I added directory log support I commited the wrong file earlier

--- application/libs/util/log.php
+++ application/libs/util/log.php
@@ -3,10 +3,11 @@
 // error message to be logged
 // setting error logging to be active
 ini_set("log_errors", TRUE);
-function simpleLog($msg, $filename = null)
+function simpleLog($msg, $directory = null, $filename = null)
 {
     // path of the log file where errors need to be logged
-    $log_file = 'logs/' . (($filename === null) ? date("Y-m-d") : $filename) . ".log";
+    if (substr($directory, -1) !== '/') $directory = $directory . '/';
+    $log_file = 'logs/' . $directory . (($filename === null) ? date("Y-m-d") : $filename) . ".log";
     // setting the logging file in php.ini
     ini_set('error_log', $log_file);
     // logging the error
