d69927e31365a15193b3748dae3f56008422fb3c
NasoohOlabi <nasooholabi@gmail.com>
2022-03-06 06:33:26 +0200

    rough pageHit counter for dashboard statistics

--- application/libs/util/log.php
+++ application/libs/util/log.php
@@ -1,15 +1,22 @@
 <?php
+// PHP code for logging error into a given file
+// error message to be logged
+// setting error logging to be active
+ini_set("log_errors", TRUE);
 function simpleLog($msg)
 {
-    // PHP code for logging error into a given file
-    // error message to be logged
     // path of the log file where errors need to be logged
-    // $log_file = "./my-errors.log";
     $log_file = 'logs/' . date("Y-m-d") . ".log";
-    // setting error logging to be active
-    ini_set("log_errors", TRUE);
     // setting the logging file in php.ini
     ini_set('error_log', $log_file);
     // logging the error
     error_log($msg);
+}
+function pageHit($page)
+{
+    $log_file = 'logs/page/' . $page . ".log";
+    // setting the logging file in php.ini
+    ini_set('error_log', $log_file);
+    // logging the error
+    error_log("$page hit");
 }
