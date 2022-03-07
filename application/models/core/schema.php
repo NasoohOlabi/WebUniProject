7eeda54cee435ddd5ddc1d11bb23264dd2390ba5
NasoohOlabi <nasooholabi@gmail.com>
2022-03-07 20:09:18 +0200

    filled in the rest of the schema

--- application/models/core/schema.php
+++ application/models/core/schema.php
@@ -380,3 +380,221 @@ class User extends Table
         }, $constants);
     }
 }
+class Student extends Table
+{
+    const id = 'student.id';
+    const enroll_date = 'student.enroll_date';
+    // this is what we'll inter act with the rest is just jargon
+    public string $enroll_date;
+
+    public function get_CRUD_Terms()
+    {
+        return ['create' => 'Enroll', 'read' => 'Take', 'update' => 'edit enrollment', 'delete' => 'Unenroll'];
+    }
+
+    public function string_identifying_columns(string $prefix = '')
+    {
+        return [0, 1];
+    }
+    function __construct($stdClass = null, $prefix = "")
+    {
+        if ($stdClass != null) {
+            $cols = Student::SQL_Columns();
+            if (properties_exists($stdClass, $cols, $prefix)) {
+                foreach ($cols as $col) {
+                    $this->$col = $stdClass->{$prefix . $col};
+                }
+            }
+        }
+    }
+    static function SQL_Columns(string $prefix = "")
+    {
+        $oClass = new ReflectionClass(__CLASS__);
+        $constants = array_keys($oClass->getConstants());
+        return ($prefix == "") ? $constants : array_map(function ($args) use ($prefix) {
+            return $prefix . $args;
+        }, $constants);
+    }
+}
+class Exam_Center extends Table
+{
+    const id = 'exam_center.id';
+    const name = 'exam_center.name';
+    const description = 'exam_center.description';
+    // this is what we'll inter act with the rest is just jargon
+    public string $name;
+    public string $description;
+
+    public function get_CRUD_Terms()
+    {
+        return ['create' => 'Enlist', 'read' => 'Take', 'update' => 'change', 'delete' => 'Decommission'];
+    }
+
+    public function string_identifying_columns(string $prefix = '')
+    {
+        return [1];
+    }
+    function __construct($stdClass = null, $prefix = "")
+    {
+        if ($stdClass != null) {
+            $cols = Exam_Center::SQL_Columns();
+            if (properties_exists($stdClass, $cols, $prefix)) {
+                foreach ($cols as $col) {
+                    $this->$col = $stdClass->{$prefix . $col};
+                }
+            }
+        }
+    }
+    static function SQL_Columns(string $prefix = "")
+    {
+        $oClass = new ReflectionClass(__CLASS__);
+        $constants = array_keys($oClass->getConstants());
+        return ($prefix == "") ? $constants : array_map(function ($args) use ($prefix) {
+            return $prefix . $args;
+        }, $constants);
+    }
+}
+class Exam_Center_Has_Exam extends Table
+{
+    const id = 'exam_center_has_exam.id';
+    const date = 'exam_center_has_exam.date';
+    const exam_id = 'exam_center_has_exam.exam_id';
+    const exam_center_id = 'exam_center_has_exam.exam_center_id';
+    // this is what we'll inter act with the rest is just jargon
+    public string $date;
+    public int $exam_id;
+    public int $exam_center_id;
+    public ?Exam $exam;
+    public ?Exam_Center $exam_center;
+    public function get_CRUD_Terms()
+    {
+        return ['create' => 'Give', 'read' => 'Take', 'update' => 'Change', 'delete' => 'Remove'];
+    }
+    public function string_identifying_columns(string $prefix = '')
+    {
+        return [0, 1];
+    }
+    function __construct($stdClass = null, $prefix = "")
+    {
+        if ($stdClass != null) {
+            $cols = Exam_Center_Has_Exam::SQL_Columns();
+            if (properties_exists($stdClass, $cols, $prefix)) {
+                foreach ($cols as $col) {
+                    $this->$col = $stdClass->{$prefix . $col};
+                }
+            }
+            $tmp = new Exam($stdClass, 'exam_');
+            if (isset($tmp->id))
+                $this->exam = $tmp;
+            $tmp = new Exam_Center($stdClass, 'exam_center_');
+            if (isset($tmp->id))
+                $this->exam_center = $tmp;
+        }
+    }
+    static function SQL_Columns(string $prefix = "")
+    {
+        $oClass = new ReflectionClass(__CLASS__);
+        $constants = array_keys($oClass->getConstants());
+        return ($prefix == "") ? $constants : array_map(function ($args) use ($prefix) {
+            return $prefix . $args;
+        }, $constants);
+    }
+}
+
+class Student_Took_Exam extends Table
+{
+    const id = 'student_took_exam.id';
+    const date = 'student_took_exam.date';
+    const choice_id = 'student_took_exam.choice_id';
+    const student_id = 'student_took_exam.student_id';
+    const exam_center_id = 'student_took_exam.exam_center_id';
+    // this is what we'll inter act with the rest is just jargon
+    public string $date;
+    public int $choice_id;
+    public int $student_id;
+    public int $exam_center_id;
+    public ?Student $student;
+    public ?Choice $choice;
+    public ?Exam_Center $exam_center;
+    public function get_CRUD_Terms()
+    {
+        return ['create' => '', 'read' => 'Take', 'update' => 'Change', 'delete' => 'Remove'];
+    }
+    public function string_identifying_columns(string $prefix = '')
+    {
+        return [0, 1];
+    }
+    function __construct($stdClass = null, $prefix = "")
+    {
+        if ($stdClass != null) {
+            $cols = Student_Took_Exam::SQL_Columns();
+            if (properties_exists($stdClass, $cols, $prefix)) {
+                foreach ($cols as $col) {
+                    $this->$col = $stdClass->{$prefix . $col};
+                }
+            }
+            $tmp = new Choice($stdClass, 'choice_');
+            if (isset($tmp->id))
+                $this->choice = $tmp;
+            $tmp = new Student($stdClass, 'student_');
+            if (isset($tmp->id))
+                $this->student = $tmp;
+            $tmp = new Exam_Center($stdClass, 'exam_center_');
+            if (isset($tmp->id))
+                $this->exam_center = $tmp;
+        }
+    }
+    static function SQL_Columns(string $prefix = "")
+    {
+        $oClass = new ReflectionClass(__CLASS__);
+        $constants = array_keys($oClass->getConstants());
+        return ($prefix == "") ? $constants : array_map(function ($args) use ($prefix) {
+            return $prefix . $args;
+        }, $constants);
+    }
+}
+
+class Exam_Has_Question extends Table
+{
+    const id = 'exam_has_question.id';
+    const question_id = 'exam_has_question.question_id';
+    const exam_id = 'exam_has_question.exam_id';
+    // this is what we'll inter act with the rest is just jargon
+    public int $question_id;
+    public int $exam_id;
+    public ?Exam $exam;
+    public ?Question $question;
+    public function get_CRUD_Terms()
+    {
+        return ['create' => 'Give', 'read' => 'Take', 'update' => 'Change', 'delete' => 'Remove'];
+    }
+    public function string_identifying_columns(string $prefix = '')
+    {
+        return [0, 1, 2];
+    }
+    function __construct($stdClass = null, $prefix = "")
+    {
+        if ($stdClass != null) {
+            $cols = Exam_Has_Question::SQL_Columns();
+            if (properties_exists($stdClass, $cols, $prefix)) {
+                foreach ($cols as $col) {
+                    $this->$col = $stdClass->{$prefix . $col};
+                }
+            }
+            $tmp = new Exam($stdClass, 'exam_');
+            if (isset($tmp->id))
+                $this->exam = $tmp;
+            $tmp = new Question($stdClass, 'question_');
+            if (isset($tmp->id))
+                $this->question = $tmp;
+        }
+    }
+    static function SQL_Columns(string $prefix = "")
+    {
+        $oClass = new ReflectionClass(__CLASS__);
+        $constants = array_keys($oClass->getConstants());
+        return ($prefix == "") ? $constants : array_map(function ($args) use ($prefix) {
+            return $prefix . $args;
+        }, $constants);
+    }
+}
